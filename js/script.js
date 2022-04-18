4a5d45b1440e4d635f27f2a37b93982149bc831a
Ashikul Islam <asikul.bhuiyan5027@gmail.com>
2022-04-18 08:58:21 +0600

    toggle side menu functionality

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
+// toggle menu item list
 let navLi = document.querySelectorAll("#navLi");
+// toggle side menu
+let header = document.querySelector("#header");
+let nav = document.querySelector("#nav");
+let navToggleBtn = document.querySelector("#navToggleBtn");
 
 navLi.forEach(function(list){
     list.addEventListener('click', function (e) {
         if (!e.currentTarget.nextElementSibling.classList.contains("active__sub--ul")) {
             resetActiveSubUl();
             e.currentTarget.nextElementSibling.classList.add("active__sub--ul");
+            header.classList.remove("deactive__nav");
+            nav.classList.remove("deactive__nav");
         } else {
-            e.currentTarget.nextElementSibling.classList.remove("active__sub--ul");            
+            e.currentTarget.nextElementSibling.classList.remove("active__sub--ul");
+            header.classList.remove("deactive__nav");
+            nav.classList.remove("deactive__nav");           
         }
     })
 });
@@ -17,3 +26,10 @@ function resetActiveSubUl() {
         list.nextElementSibling.classList.remove("active__sub--ul");
     });
 }
+
+// toggle side menu functionlity
+navToggleBtn.addEventListener('click', function (e) {
+    header.classList.toggle("deactive__nav");
+    nav.classList.toggle("deactive__nav");
+    resetActiveSubUl();
+})
