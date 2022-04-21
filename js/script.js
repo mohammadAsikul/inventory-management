1705c007a2536397d2b760ecf148b667b7e385a1
Ashikul <asikul.bhuiyan5027@gmail.com>
2022-04-22 02:07:32 +0600

    add new item row facing problems

--- js/script.js
+++ js/script.js
@@ -5,6 +5,9 @@ let header = document.querySelector("#header");
 let nav = document.querySelector("#nav");
 let navToggleBtn = document.querySelector("#navToggleBtn");
 let mainContent = document.querySelector("#main");
+// invoice new line adding
+let invoiceItemTable = document.querySelector("#invoiceItemTable");
+let addNewRow = document.querySelector("#addNewRow");
 
 navLi.forEach(function(list){
     list.addEventListener('click', function (e) {
@@ -36,4 +39,13 @@ navToggleBtn.addEventListener('click', function (e) {
     nav.classList.toggle("deactive__nav");
     mainContent.classList.toggle("deactive__nav");
     resetActiveSubUl();
+})
+
+// invoice new line adding
+let addLineHtml = '<tr><td><select name="itemSelectBox" id="itemSelectBox"><option value="" disabled selected>select your item</option></select></td><td>1</td><td><input type="number" name="itemRate" id="itemRate" placeholder="item rate" value=""></td><td><input type="number" name="itemQty" id="itemQty" placeholder="item qty" value=""></td><td><input type="number" name="itemDiscount" id="itemDiscount" placeholder="item discount" value=""></td><td>100</td><td><button id="itemDeleteBtn" class="item__delete--btn"><i class="fa-solid fa-trash"></i></button></td></tr>';
+
+let x = 1;
+addNewRow.addEventListener('click', function (e) {
+    // invoiceItemTable.appendChild(addLineHtml);
+    console.log("hello you click me!");
 })
