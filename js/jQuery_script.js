87018d2ebd3feaebf0191140cc7b276963dd99fc
Ashikul <asikul.bhuiyan5027@gmail.com>
2022-04-29 04:39:42 +0600

    date: 29.04.2022 04:40 AM

--- js/jQuery_script.js
+++ js/jQuery_script.js
@@ -1,9 +1,3 @@
 $(document).ready(function () {
-    // var html = '<tr><td><select name="itemSelectBox" id="itemSelectBox"><option value="" disabled selected>select your item</option></select></td><td>1</td><td><input type="number" name="itemRate" id="itemRate" placeholder="item rate" value=""></td><td><input type="number" name="itemQty" id="itemQty" placeholder="item qty" value=""></td><td><input type="number" name="itemDiscount" id="itemDiscount" placeholder="item discount" value=""></td><td>100</td><td><button id="itemDeleteBtn" class="item__delete--btn"><i class="fa-solid fa-trash"></i></button></td></tr>';
-    var html = "Hello";
-
-    var x = 1;
-    $("#addNewRow").click(function () {
-        $("#invoiceItemTable").append(html);
-    })
+    
 });
--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 // toggle menu item list
 let navLi = document.querySelectorAll("#navLi");
+
 // toggle side menu
 let header = document.querySelector("#header");
 let nav = document.querySelector("#nav");
 let navToggleBtn = document.querySelector("#navToggleBtn");
 let mainContent = document.querySelector("#main");
+
 // invoice new line adding
 let invoiceItemTable = document.querySelector("#invoiceItemTable");
+let newInvoiceTbody = document.querySelector("#newInvoiceTbody");
 let addNewRow = document.querySelector("#addNewRow");
 
 navLi.forEach(function(list){
@@ -40,12 +43,17 @@ navToggleBtn.addEventListener('click', function (e) {
     mainContent.classList.toggle("deactive__nav");
     resetActiveSubUl();
 })
-
-// invoice new line adding
-let addLineHtml = '<tr><td><select name="itemSelectBox" id="itemSelectBox"><option value="" disabled selected>select your item</option></select></td><td>1</td><td><input type="number" name="itemRate" id="itemRate" placeholder="item rate" value=""></td><td><input type="number" name="itemQty" id="itemQty" placeholder="item qty" value=""></td><td><input type="number" name="itemDiscount" id="itemDiscount" placeholder="item discount" value=""></td><td>100</td><td><button id="itemDeleteBtn" class="item__delete--btn"><i class="fa-solid fa-trash"></i></button></td></tr>';
-
 let x = 1;
+// invoice new line adding
 addNewRow.addEventListener('click', function (e) {
-    // invoiceItemTable.appendChild(addLineHtml);
-    console.log("hello you click me!");
+    e.preventDefault();
+    newInvoiceTbody.innerHTML += `
+    <tr><td>${x = x+1}</td><td><select name="itemSelectBox" id="itemSelectBox"><option value="" disabled selected>select your item</option></select></td><td>1</td><td><input type="number" name="itemRate" id="itemRate" placeholder="item rate" value=""></td><td><input type="number" name="itemQty" id="itemQty" placeholder="item qty" value=""></td><td><input type="number" name="itemDiscount" id="itemDiscount" placeholder="item discount" value=""></td><td>100</td><td><button id="itemDeleteBtn" class="itemDeleteBtn"><i class="fa-solid fa-trash"></i></button></td></tr>
+    `;
+});
+// delete invoice item from table
+let removeBtn = document.querySelector(".itemDeleteBtn");
+removeBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    console.log(this);
 })
